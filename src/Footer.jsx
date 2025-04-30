571b904b898778329322dc812ec4171afa48d3f6
Sachin Mahapatra <sachinmahapatra858@gmail.com>
2025-04-30 12:11:00 +0530

    signup with google

---
+++ src/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        
+        <div className="mt-8 pt-8 border-t text-center text-gray-400">
+          <p>© 2025 Assignment by Sachin Mahapatra. </p>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
